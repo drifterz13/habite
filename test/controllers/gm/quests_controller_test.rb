e580133366eb6639907474a059e2d0103af1cce5
drifterz13 <mixmyxxe@gmail.com>
2025-08-17 14:41:46 +0700

    fix: cannot create quest

--- test/controllers/gm/quests_controller_test.rb
+++ test/controllers/gm/quests_controller_test.rb
@@ -12,11 +12,8 @@ class Gm::QuestsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create quest" do
-    assert_difference("Quest.count") do
-      start_at = Time.now + 1.days
-      end_at = start_at + 1.days
-
-      post gm_quests_url, params: { quest: { title: "Asking peer to review work", description: "Request for MR review", end_at:, start_at: } }
+    assert_difference -> { Quest.count }, 1 do
+      post gm_quests_url, params: { quest: { title: "Asking peer to review work" } }
     end
 
     assert_redirected_to quest_url(Quest.last)
@@ -24,7 +21,7 @@ class Gm::QuestsControllerTest < ActionDispatch::IntegrationTest
 
   test "should destroy quest" do
     quest = quests(:in_progress)
-    assert_difference("Quest.count", -1) do
+    assert_difference -> { QUest.count }, -1 do
       delete gm_quest_url(quest)
     end
 
